3b865c792e85c4f248e107aafd7972841de6246e
Paul JULIEN <paul.julien.dev@gmail.com>
2019-04-03 11:44:45 -0400

    feature #1 - Args with argparse, need to manage paths

--- src/pyLS.py
+++ src/pyLS.py
@@ -9,9 +9,29 @@ def recursive_option():
     print("this is recursive")
 
 
+def long_option():
+    print("this is long")
+
+
+def count_option():
+    print("this is count")
+
+
+def directory_option():
+    print("this is directory")
+
+
+def reverse_option():
+    print("this is reverse")
+
+
 OPTION_MAP = {
     'all': all_option,
-    'Recursive': recursive_option
+    'Recursive': recursive_option,
+    'long': long_option,
+    'count': count_option,
+    'directory': directory_option,
+    'reverse': reverse_option
 }
 
 
@@ -19,8 +39,8 @@ def manage_options():
     parser = argparse.ArgumentParser(prog='pyLS.py')
     parser.add_argument('-a', '--all', action='store_true', help='Display additional hidden files or directories')
     parser.add_argument('-R', '--Recursive', action='store_true', help='Recursive display')
-    parser.add_argument('-l', action='store_true', help='Display the size of files or directories')
-    parser.add_argument('-c', action='store_true', help='Display number of lines in the file')
+    parser.add_argument('-l', '--long', action='store_true', help='Display the size of files or directories')
+    parser.add_argument('-c', '--count', action='store_true', help='Display number of lines in the file')
     parser.add_argument('-d', '--directory', action='store_true',
                         help='Display only directories, and the number of file in each')
     parser.add_argument('-r', '--reverse', action='store_true', help='Reverse the display order')
@@ -31,7 +51,7 @@ def manage_options():
     for key, value in d.items():
         if value:
             OPTION_MAP[key]()
-    print(d)
+#    print(d)
 
 
 def main():

a105babb1b7aeb0471b4341ff0d3c15e8cf19d8e
Paul JULIEN <paul.julien.dev@gmail.com>
2019-04-06 18:10:59 -0400

    feature #3 - edit test

--- test/unit/testOptions.py
+++ test/unit/testOptions.py
@@ -2,7 +2,7 @@ import unittest
 import os
 
 from src.pyLS_pkg.classes.Directory import Directory
-from src.pyLS_pkg.options import get_dirs_and_files, directory_option, long_option, all_option, count_option
+from src.pyLS_pkg.options import get_dirs_and_files, directory_option, all_option
 
 
 class TestOptions(unittest.TestCase):
@@ -13,7 +13,7 @@ class TestOptions(unittest.TestCase):
         get_dirs_and_files(self.directory)
         all_option(self.directory)
         self.assertEqual(len(self.directory.get_directories()),
-                         sum(os.path.isdir(i) for i in os.listdir(self.directory.get_path()) if i[0] != '.'))
+                         sum(os.path.isdir(i) for i in os.listdir(self.directory.get_path())) + 2)
 
     def test_directory_option(self):
         get_dirs_and_files(self.directory)

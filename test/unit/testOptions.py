89b1ace195b57cd8ab0ba7872f81038075d59175
Paul JULIEN <paul.julien.dev@gmail.com>
2019-04-06 17:39:45 -0400

    feature #3 - wrote some tests

---
+++ test/unit/testOptions.py
@@ -0,0 +1,21 @@
+import unittest
+import os
+
+from src.pyLS_pkg.classes.Directory import Directory
+from src.pyLS_pkg.options import get_dirs_and_files, directory_option, long_option, all_option, count_option
+
+
+class TestOptions(unittest.TestCase):
+    def setUp(self):
+        self.directory = Directory('root', '.', False, False)
+
+    def test_all_option(self):
+        get_dirs_and_files(self.directory)
+        all_option(self.directory)
+        self.assertEqual(len(self.directory.get_directories()),
+                         sum(os.path.isdir(i) for i in os.listdir(self.directory.get_path()) if i[0] != '.'))
+
+    def test_directory_option(self):
+        get_dirs_and_files(self.directory)
+        directory_option(self.directory)
+        self.assertEqual(0, len(self.directory.get_files()))
